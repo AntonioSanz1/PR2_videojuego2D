d43d084a180e3d5c7ba4aa65a9b2992d317a146f
AntonioSanz1 <antoniosanz97@gmail.com>
2025-03-25 14:35:54 +0100

    Moví el personaje

--- Assets/Scrpits/Saltobien.cs
+++ Assets/Scrpits/Saltobien.cs
@@ -13,17 +13,97 @@ public class Saltobien : MonoBehaviour
 
     public GameObject miCubo;
 
+    public int numero1 = 10;
+    public int numero2 = 80;
+
+    public Vector3 posinicial;
+
+    public Color miColor;
+
+    
+    public float velocidad = 0.1f;
+
 
     // Start is called before the first frame update
     void Start()
     {
+
+        this.GetComponent<Transform>().position = posinicial;
+
+        Vector3 posiniticonfutura = new Vector3(100,10,0);
+
+        bool tum = true;
+
+        tum = this.GetComponent<SpriteRenderer>().flipX;
+        
+        //this.GetComponent<SpriteRenderer>().color = miColor;
+
+
+        
+
     }
+    
+     
+    
+    
+        int Sumar(int numero1, int numero2)
+        {
+       
+            int resultado = numero1+numero2;
+            return resultado;
+        
+
+        }
+       
+
+    
+        
+
 
     // Update is called once per frame
     
     void Update()
     {
-                Debug.Log("Hola. Mi nombre es "+miNombre+". Y tengo "+miEdad+" años"); 
+        Debug.Log("Hola. Mi nombre es "+miNombre+". Y tengo "+miEdad+" años"); 
+
+        /*float positionActual = this.GetComponent<Transform>().position.x; //x,y,z
+        transform.position = new Vector3(1+0.1f,0,0)*/
+
+        //transform.Translate(0,1+0.1f,0);
+
+        //transform.Translate(velocidad,0,0);
+        
+        //Debug.Log(transform.rotation); quaterniones
+        // euler = leer en grados 
+        //Debug.Log(transform.eulerAngles);
+
+        //transform.Rotate(0,0,velocidad);
+
+       // transform.localScale = new Vector3(2,1,2);
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(velocidad* -1,0,0);
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(velocidad,0,0);
+        }
+  
 
     }
+    /*void OnGUI()
+    {
+         Debug.Log(Event.current.type.ToString());
+
+         if(Event.current.isMouse && Event.current.button == 0)
+         {
+            Debug.LogFormat(Event.current.type.ToString());
+
+         }
+    }*/
+
+
 }
